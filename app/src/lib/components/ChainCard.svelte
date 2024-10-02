e8a926c5964155f11bfc044fc5aa683a216e9caa
code0xff <ian.jungyong.um@gmail.com>
2024-10-03 00:49:51 +0900

    feat: Add chain for metamask

--- app/src/lib/components/ChainCard.svelte
+++ app/src/lib/components/ChainCard.svelte
@@ -6,17 +6,17 @@
 	export let onclick;
 </script>
 
-<div class="my-1 flex w-full flex-row items-start rounded-xl bg-gray-100 p-4 dark:bg-gray-800">
-	<div class="flex flex-1 flex-row">
-		<img src={image} class="my-auto h-10 w-10" alt={image} />
-		<p class="my-auto pl-4 text-lg font-medium dark:text-white">{walletName}</p>
+<div class="my-2 flex w-full flex-row items-start rounded-xl bg-gray-100 p-2 dark:bg-gray-800">
+	<div class="my-auto flex flex-1 flex-row">
+		<img src={image} class="my-auto h-8 w-8" alt={image} />
+		<p class="text-md my-auto pl-2 font-medium dark:text-white">{walletName}</p>
 	</div>
 	<div class="flex flex-1 flex-col">
-		<p class="text-md flex-1dark:text-white my-auto">{denom}</p>
-		<p class="my-auto flex-1 text-sm text-gray-500 dark:text-gray-400">{chainName}</p>
+		<p class="my-auto flex-1 text-sm dark:text-white">{denom}</p>
+		<p class="my-auto flex-1 text-xs text-gray-500 dark:text-gray-400">{chainName}</p>
 	</div>
 	<button
-		class="my-auto flex-1 cursor-pointer text-left text-blue-600 hover:opacity-80 dark:text-blue-400"
+		class="my-auto flex-1 cursor-pointer text-left text-sm text-blue-600 hover:opacity-80 dark:text-blue-400"
 		on:click={onclick}
 	>
 		{'Add to Wallet >'}
--- app/src/lib/pages/Chain/ChainList.svelte
+++ app/src/lib/pages/Chain/ChainList.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import ChainCard from '$lib/components/ChainCard.svelte';
-	import { suggestChain as suggestChainToKeplr } from './services/keplr';
+	import { suggestChain } from './services/keplr';
+	import { addEthereumChain } from './services/metamask';
 </script>
 
 <div class="w-full">
-	<div class="flex w-full flex-row">
+	<div class="flex w-full flex-row px-2">
 		<p class="flex-1 text-xs text-gray-500 dark:text-gray-400">WALLET NAME</p>
 		<p class="flex-1 text-xs text-gray-500 dark:text-gray-400">CHAIN INFO</p>
 		<p class="flex-1 text-xs text-gray-500 dark:text-gray-400">ADD WALLET</p>
@@ -14,6 +15,13 @@
 		walletName={'Keplr'}
 		chainName={'Ziggurat'}
 		denom={'ZIG'}
-		onclick={suggestChainToKeplr}
+		onclick={suggestChain}
+	/>
+	<ChainCard
+		image={'/metamask.png'}
+		walletName={'MetaMask'}
+		chainName={'Ziggurat'}
+		denom={'ZIG'}
+		onclick={addEthereumChain}
 	/>
 </div>
