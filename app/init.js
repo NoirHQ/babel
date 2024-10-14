b9c4d0c06978e1c26537536546e1f7b17ecc32ee
Jeeyong Um <conr2d@proton.me>
2024-10-14 16:00:27 +0900

    chore: Update init chain script

--- app/init.js
+++ app/init.js
@@ -39,31 +39,47 @@ async function main() {
 		minBalance: '1000000000000000'
 	};
 
+	const args = { api, sudo, owner, amount, RAT };
+
 	const unsubSudo = await api.tx.sudo
 		.sudo(api.tx.babel.forceCreateAsset(...Object.values(RAT)).inner.toHex())
 		.signAndSend(sudo, async (result) => {
 			if (result.status.isInBlock) {
 				unsubSudo();
 				console.log(`Created     | RatCoin (id: ${RAT.id}, symbol: RAT)`);
-				const unsubTransfer = await api.tx.balances
-					.transferKeepAlive(owner.address, '1000000000000000000')
-					.signAndSend(sudo, async (result) => {
-						if (result.status.isInBlock) {
-							unsubTransfer();
-							console.log(`Transferred | 1 ZIG to ${owner.address} for tx fees.`);
-							const unsubMint = await api.tx.assets
-								.mint(RAT.id, owner.address, amount)
-								.signAndSend(owner, (result) => {
-									if (result.status.isInBlock) {
-										unsubMint();
-										console.log(
-											`Minted      | ${amount} RAT to ${owner.address}.`
-										);
-										process.exit(0);
-									}
-								});
-						}
-					});
+				if (sudo.address != owner.address) {
+					await fund(args);
+				} else {
+					await mint(args);
+				}
+			}
+		});
+}
+
+async function fund(args) {
+	const { api, sudo, owner } = args;
+	const unsubTransfer = await api.tx.balances
+		.transferKeepAlive(owner.address, '1000000000000000000')
+		.signAndSend(sudo, async (result) => {
+			if (result.status.isInBlock) {
+				unsubTransfer();
+				console.log(`Transferred | 1 ZIG to ${owner.address} for tx fees.`);
+				await mint(args);
+			}
+		});
+}
+
+async function mint(args) {
+	const { api, RAT, owner, amount } = args;
+	const unsubMint = await api.tx.assets
+		.mint(RAT.id, owner.address, amount)
+		.signAndSend(owner, (result) => {
+			if (result.status.isInBlock) {
+				unsubMint();
+				console.log(
+					`Minted      | ${amount} RAT to ${owner.address}.`
+				);
+				process.exit(0);
 			}
 		});
 }
